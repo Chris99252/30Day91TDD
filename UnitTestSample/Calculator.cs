a4c83bc5df7dda3a1a61954aea15204318463d32
Chris99252 <chris@ccf.org.tw>
2015-07-22 21:13:30 +0800

    [30天快速上手TDD][Day 3]動手寫 Unit Test

---
+++ UnitTestSample/Calculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestSample
+{
+    public class Calculator
+    {
+        public int Add(int firstNumber, int secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+
+        public void SomeMethod()
+        {
+            throw new Exception();
+        }
+
+
+        public int Minus(int firstNumber, int secondNumber)
+        {
+            return firstNumber - secondNumber;
+        }
+    }
+}
